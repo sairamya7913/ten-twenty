47e8404a1d4612582742da7df0c5a92ebba78d3c
sairamya7913 <sairamyakasu87@gmail.com>
2025-10-15 20:23:39 +0530

    Updated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 
